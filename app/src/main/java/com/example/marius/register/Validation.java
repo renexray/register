13206828f7e188a84d133a30ed2f840501e7825f
renexray <renexray@users.noreply.github.com>
2018-02-15 10:22:47 +0200

    Rejected commit: Default

--- app/src/main/java/com/example/marius/register/Validation.java
+++ app/src/main/java/com/example/marius/register/Validation.java
@@ -9,8 +9,7 @@ import java.util.regex.Pattern;
 
 public class Validation {
     private static final String VALID_CREDENTIALS_REGEX ="^[A-Za-z0-9.-]{5,13}$";
-    private static final String VALID_EMAIL_ADDRESS_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
-
+    private static final String VALID_EMAIL_ADDRESS_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
 
     public static boolean isValidCredentials(String credentials){
         Pattern credentialsPattern = Pattern.compile(VALID_CREDENTIALS_REGEX);
